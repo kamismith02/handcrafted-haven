9359d5d5a50a6dc47f2234fc7e9740b123b5d590
amandamigliorini <amanda.migliorini@hotmail.com>
2024-12-19 22:31:13 -0300

    minor adjustment

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,23 +1,19 @@
 import { sql } from '@vercel/postgres';
 import { getSession } from 'next-auth/react';
-import { NextRequest, NextResponse } from 'next/server'; // Importe as tipagens apropriadas
+import { NextRequest, NextResponse } from 'next/server';
 
-// Tipagem correta para 'request' com NextRequest
 export async function POST(request: NextRequest) {
-  const session = await getSession({ req: request as any }); // Cast de 'request' para 'any'
+  const session = await getSession({ req: request as any });
 
-  // Verifique se a sessão existe e se o ID do usuário está presente
   if (!session || !session.user || !session.user.id) {
     return new NextResponse('User is not authenticated', { status: 401 });
   }
 
   const { comment, rating, productId } = await request.json();
 
-  // Obtenha o ID do usuário da sessão
   const userId = session.user.id;
 
   try {
-    // Agora, insira o review com o userId
     await sql`
       INSERT INTO reviews (user_id, product_id, comments, rate)
       VALUES (${userId}, ${productId}, ${comment}, ${rating})
